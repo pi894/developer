c388af399df5b4cf02c37c9a1bc8cb09c43b8932
piyush <piyushsinha916@gmail.com>
2020-08-21 14:18:05 +0530

    1st

--- src/test/java/sample/sample123/TestClass.java
+++ src/test/java/sample/sample123/TestClass.java
@@ -1,30 +1,25 @@
 package sample.sample123;
 
-import org.openqa.selenium.WebDriver;
+/*import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;*/
 import org.testng.annotations.Test;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import pageObjectsPackage.LoginPage;
-import pageObjectsPackage.NewUserRegistrationPage;
+/*import pageObjectsPackage.LoginPage;
+import pageObjectsPackage.NewUserRegistrationPage;*/
 import java.util.concurrent.TimeUnit;
 
 
 public class TestClass {
-	public static WebElement element = null;
-	String baseUrl;
-	WebDriver driver;
+//	public static WebElement element = null;
+	//String baseUrl;
+	//WebDriver driver;
 	
 	
 	@BeforeMethod
 	public void SetUp() {
-		baseUrl = "http://thawing-shelf-73260.herokuapp.com/";
-		System.setProperty("webdriver.gecko.driver", "C:\\geko\\geckodriver.exe");
-		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
-		driver.get(baseUrl);
+		
 	}
 
 	
@@ -33,7 +28,7 @@ public class TestClass {
 	 * Test cases based on User story-007:- 
 	 * As a user concerned about security and interested in personal finance, 
 	 * user can registered in order to have access to the expense tracker functionalities.
-	 */
+	 
 	
 	@Test(dataProvider="newUserData", dataProviderClass=DataProviderClass.class)
 	public void testcase1_create_new_user(String username, String password, String confirmpassword) {
@@ -43,23 +38,23 @@ public class TestClass {
 	
 	
 //*****************************************************************************************************	
-	/*
+	
 	 * Test cases based on User story-006:- 
 	 * As a user concerned about security, user can access to all functionalities 
 	 * related to expense tracking if he/she is a registered user and logged in.
-	 */
+	 
 	
 	@Test(dataProvider="loginData", dataProviderClass=DataProviderClass.class)
 	public void testcase2_login_to_tracker(String username, String password) {
 		LoginPage.loginToExpenseTracker(driver, username, password);
 	}
-
+*/
 //*****************************************************************************************************	
 
 	
 	@AfterMethod
 	public void tearDown() {
-		driver.quit();
+	//	driver.quit();
 	}
 }
 
